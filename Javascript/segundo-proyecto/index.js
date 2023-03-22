90d93cacacd8af8c648cbd5c36cf59099ad657b4
Valentin Alterio <alteriovalentin01@gmail.com>
2023-03-22 11:29:27 -0300

    Segundo ejercicio

---
+++ Javascript/segundo-proyecto/index.js
@@ -0,0 +1,12 @@
+const lista = [
+  "valentin",
+  21,
+  true,
+  new Date(2001 / 07 / 10),
+  {
+    titulo: "El Hombre más rico de Babilonia",
+    autor: "George S. Clason",
+    fecha: new Date(2020 / 4 / 22),
+    url: "https://www.amazon.es/Hombre-m%C3%A1s-rico-Babilonia-Obelisco/dp/8491114548/ref=tmm_pap_swatch_0?_encoding=UTF8&qid=1674783917&sr=1-1",
+  },
+];
