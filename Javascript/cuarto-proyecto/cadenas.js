f1a35c6a2bbcdc255ed5d9bb3afb111fad89b582
Valentin Alterio <alteriovalentin01@gmail.com>
2023-04-18 20:07:15 -0300

    cuarto ejercicio

---
+++ Javascript/cuarto-proyecto/cadenas.js
@@ -0,0 +1,17 @@
+let nombre = "Valentin";
+let apellido = "Alterio";
+let estudiante = `${nombre} ${apellido}`;
+let estudianteMayus = estudiante.toUpperCase();
+//console.log(estudianteMayus)
+let estudianteMinus = estudiante.toLowerCase();
+//console.log(estudianteMinus)
+let cant_letras = estudiante.length;
+//console.log(cant_letras)
+let pr_letra = nombre.startsWith();
+
+let ul_letra = apellido.endsWith();
+
+let sin_espacios = estudiante.trim();
+console.log(sin_espacios);
+let bool = nombre.includes("estudiante");
+console.log(bool);
