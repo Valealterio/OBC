5bf4378c8d239335f5c40bbcaf9f08dfa3ab322a
Valentin Alterio <alteriovalentin01@gmail.com>
2023-05-23 12:34:56 -0300

    ejercicio 13

---
+++ Javascript/proyecto-13/index.js
@@ -0,0 +1,17 @@
+const apellido = "Alterio"
+const nombre = "Valentin"
+const estudiante = `${nombre} ${apellido}`
+const estudianteMayus = estudiante.toUpperCase()
+//console.log(estudianteMayus)
+const estudianteMinus = estudiante.toLowerCase()
+//console.log(estudianteMinus)
+const cant_letras = estudiante.length
+//console.log(cant_letras)
+const pr_letra = nombre.startsWith()
+
+const ul_letra = apellido.endsWith()
+
+let sin_espacios = estudiante.trim()
+console.log(sin_espacios)
+let bool = nombre.includes("estudiante")
+console.log(bool)
