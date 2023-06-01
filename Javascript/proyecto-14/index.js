f9494049e463ff9a8e6a6e5a1d23992d0db51d3b
Valentin Alterio <alteriovalentin01@gmail.com>
2023-06-01 11:36:42 -0300

    correcion 14

--- Javascript/proyecto-14/index.js
+++ Javascript/proyecto-14/index.js
@@ -1,10 +1,9 @@
-const boton = document.getElementById("btn")
+const boton = document.querySelector("#btn");
 
 boton.addEventListener("click", () => {
-    alert("click en el botón")
-})
+    alert("click en el botón");
+});
 
-$("btn").click(() =>{ 
-    $("btn").hide()
-    console.log("Hola, estoy utilizando jQuery")
-})
+$("#btn").click(() => { 
+    console.log("Hola, estoy utilizando jQuery");
+});
