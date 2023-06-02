dd49f51d6f15480e69f183c91f2442a5d3afe3a0
Valentin Alterio <alteriovalentin01@gmail.com>
2023-06-01 23:42:08 -0300

    ejercicio 15

---
+++ Javascript/proyecto-15/index.js
@@ -0,0 +1,9 @@
+let nombre = "Valentin"
+let apellido = "Alterio"
+let NyA = {
+    nombre: nombre,
+    apellido: apellido
+}
+//sessionStorage.setItem("persona", JSON.stringify(NyA))
+//localStorage.setItem("persona", JSON.stringify(NyA))
+//document.cookie = "personita=NyA;expires=" + new Date(120000).toUTCString
